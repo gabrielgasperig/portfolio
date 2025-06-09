6e5cd313d773f3da17a907d6e8f4a00a247ba884
Gabriel Gasperi Gawadryn <gaabrielgasperi@gmail.com>
2025-06-08 23:58:51 -0300

    adicionando backend do formulario de contato e footer

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-/* Abre e fecha menu lateral em modo mobile */
+// Abre e fecha menu lateral em modo mobile
 
 const menuMobile = document.querySelector('.menu-mobile');
 const body = document.querySelector('body');
@@ -10,7 +10,7 @@ menuMobile.addEventListener('click', () => {
     body.classList.toggle("menu-nav-active");
 });
 
-/* Fecha o menu lateral ao clicar em algum item e muda para o icone list */
+// Fecha o menu lateral ao clicar em algum item e muda para o icone list 
 
 const navItem = document.querySelectorAll(".nav-item");
 
@@ -44,3 +44,28 @@ animeScroll();
 window.addEventListener("scroll", ()=>{
   animeScroll();
 })
+
+// Ativar carregamento no botão de enviar formulário
+
+const form = document.querySelector('#formulario');
+const btnEnviar = document.querySelector('#btn-enviar');
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader');
+
+form.addEventListener('submit', function (e) {
+  if (form.checkValidity()) {
+    // formulário válido: ativa o loader
+    btnEnviar.style.display = 'none';
+    btnEnviarLoader.style.display = 'inline-block';
+  } else {
+    // formulário inválido: impede envio
+    e.preventDefault(); // cancela envio
+    form.reportValidity(); // exibe mensagens de erro
+  }
+});
+
+
+// tira mensagem de succeso depois de 5s
+
+setTimeout(() => {
+  document.querySelector('#alerta').style.display = 'none';
+}, 5000)
